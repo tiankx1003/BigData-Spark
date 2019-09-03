c8b7903c5883393913d755cebeb52947ad783d00
Tiankx1003 <tiankx1003@gmail.com>
2019-09-03 20:57:47 +0800

    scala day01

--- scala/src/main/scala/com/tian/preview/day01/HelloWorld.scala
+++ scala/src/main/scala/com/tian/preview/day01/HelloWorld.scala
@@ -1,9 +1,17 @@
 package com.tian.preview.day01
 
 /**
- * @user tian
+ * @author tian
  *       2019/9/3 19:28
  */
-class HelloWorld {
-
+object HelloWorld {
+    def main(args: Array[String]): Unit = {
+        System.out.println("Hello World!")
+        println("Hello World!")
+        printf("Hello World!")
+        val a = 10
+        val b = 10.1
+        val s = "Hello World!"
+        printf("Print: %d %f %.2f %s",a,b,b,s)
+    }
 }
--- scala/src/main/scala/com/tian/preview/day01/IfDemo.scala
+++ scala/src/main/scala/com/tian/preview/day01/IfDemo.scala
@@ -4,6 +4,19 @@ package com.tian.preview.day01
  * @author tian
  *         2019/9/3 20:31
  */
-class IfDemo {
+object IfDemo {
+    def main(args: Array[String]): Unit = {
+        val m = 10
+        val n = 20
+        if (m > n) {
+            println(m)
+        } else if (m < n) {
+            println(n)
+        } else {
+            println("equal")
+        }
 
+        val j = if (3 < 2) 10 else if (3 < 1) 20 else 30
+        val i = if (m > n) m else n;
+    }
 }
