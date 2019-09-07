1c54802cd76a33071805b77f86f6c9a75cd2e6d8
Tiankx1003 <tiankx1003@gmail.com>
2019-09-07 17:20:51 +0800

    scala day04

---
+++ scala/src/main/scala/com/tian/onclass/day04/MakeString.scala
@@ -0,0 +1,12 @@
+package com.tian.onclass.day04
+
+/**
+ * @author tian
+ *         2019/9/7 17:15
+ */
+object MakeString {
+    def main(args: Array[String]): Unit = {
+        val arr = Array(1,2,3,4)
+        println(arr.mkString("."))
+    }
+}
