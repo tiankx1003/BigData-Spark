546a86a32a25924da520517322e4f2e5eddead46
Tiankx1003 <Tiankx1003@gmail.com>
2019-09-09 01:43:52 +0800

    exam

--- scala/src/main/scala/com/tian/review/day04/exam/ReduceDemo.scala
+++ scala/src/main/scala/com/tian/review/day04/exam/ReduceDemo.scala
@@ -1,5 +1,7 @@
 package com.tian.review.day04.exam
 
+import scala.reflect.io.File
+
 
 /**
  *
@@ -7,32 +9,66 @@ package com.tian.review.day04.exam
  * @version 1.0
  *          2019/9/9 0:35
  */
-/*
-1. 使用 reduce 计算集合中的最大值
-
-2. 使用 foldLeft 同时计算最大值和最小值
-
-3. 编写一个函数, 接收一个字符串, 返回一个 Map.
-    比如: indexes("Helloee")
-
-   返回: Map(H->{0}, e -> {1, 5, 6}, ...)   数字其实是下标
-
-4. 实现一个函数，作用与mkString相同，使用foldLeft
-
-5. 实现一个 scala 版的 wordcount
-   读取文件, 然后实现
-   Source.
- */
 object ReduceDemo {
     def main(args: Array[String]): Unit = {
         println(reduceMax(List(1, 4, 6, 2)))
+        println(maxMin(List(10, 2, 44, 2, 10, 1, 56)))
     }
 
-    def reduceMax(arr: List[Int]) = arr.reduce(math.max(_, _))
+    /**
+     * 1. 使用 reduce 计算集合中的最大值
+     *
+     * @param list
+     * @return
+     */
+    def reduceMax(list: List[Int]) = list.reduce(math.max(_, _))
 
-    def maxMin(arr: List[Int]) = {
-        arr.foldLeft()
+    /**
+     * 2. 使用 foldLeft 同时计算最大值和最小值
+     *
+     * @param list
+     * @return
+     */
+    def maxMin(list: List[Int]): List[Int] = {
+        list.foldLeft(List(list(0), list(0)))((a: List[Int], b: Int) =>
+            List(math.min(a(0), b), math.max(a(1), b))
+        )
     }
+
+    /**
+     * 3. 编写一个函数, 接收一个字符串, 返回一个 Map.
+     * 比如: indexes("Helloee")
+     *
+     * @param str
+     */
+    def strMap(str: String) = {
+
+    }
+
+    /**
+     * 4. 实现一个函数，作用与mkString相同，使用foldLeft
+     *
+     * @param arr
+     * @tparam T
+     * @return
+     */
+    def makeString[T](arr: Array[T]): String = {
+
+        ""
+    }
+
+    /**
+     * 5. 实现一个 scala 版的 wordcount
+     * 读取文件, 然后实现
+     * Source.
+     *
+     * @param file
+     * @return
+     */
+    def fileWorldCount(file: File): Map[String, Int] = {
+        null
+    }
+
 }
 
 
