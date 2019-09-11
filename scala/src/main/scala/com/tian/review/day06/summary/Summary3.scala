2223e7aa3fc80e602031e48e6526ce8fd25eeafe
Tiankx1003 <tiankx1003@gmail.com>
2019-09-11 19:24:07 +0800

    spark wordcount

--- scala/src/main/scala/com/tian/review/day06/summary/Summary3.scala
+++ scala/src/main/scala/com/tian/review/day06/summary/Summary3.scala
@@ -1,11 +1,31 @@
 package com.tian.review.day06.summary
 
 /**
+ * apply & update
+ *
  * @author tian
  *         2019/9/10 21:19
  */
 object Summary3 {
     def main(args: Array[String]): Unit = {
         //TODO 当前进度
+        val user1 = new User("lisi",20)
+        User() //调用伴生对象的apply方法
+        user1("name") //调用伴生类中的抽象方法
+        user1("lisi") = "wangwu" //调用伴生类中的apply方法
+        !user1 //一元运算符
+        (user1.fun _).apply
     }
+}
+
+class User(var name: String, var age: Int){
+    def apply(propName: String) = println(propName)
+    def update(propName: String, v: String) =
+        if(propName == "name") this.name = v
+    def unary_! = println("Hello")
+    def fun() = println("fun...")
+}
+
+object User{
+    def apply() = println("object apply...")
 }
---
+++ spark/spark-core/src/main/scala/com/tian/day01/WordCount.scala
@@ -0,0 +1,36 @@
+package com.tian.day01
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * WordCount
+ * com.tian.day01.WordCount
+ *
+ * @author tian
+ *         2019/9/11 16:46
+ */
+object WordCount {
+    def main(args: Array[String]): Unit = {
+
+        //1.初始化spark context
+        val conf = new SparkConf()
+            .setAppName("WordCount")
+            .setMaster("local[*]") //上传jar包在服务器运行不能设置
+        val sc = new SparkContext(conf)
+
+        //2.转换
+        val rdd = sc.textFile(args(0)) //手动传入路径
+            .flatMap(_.split("\\W+"))
+            .map((_, 1))
+            .reduceByKey(_ + _)
+
+        rdd.saveAsTextFile(args(1)) //传入输出结果的路径
+
+        //3.行动
+        val result = rdd.collect()
+        result.foreach(println)
+
+        //4.关闭上下文
+        sc.stop()
+    }
+}
