a155a07e8e7c38511a2494ddf78feba31674ad47
Tiankx1003 <tiankx1003@gmail.com>
2019-09-18 08:52:16 +0800

    file read & write

--- spark/spark-core/src/main/scala/com/tian/day04/hbase/HBaseRead.scala
+++ spark/spark-core/src/main/scala/com/tian/day04/hbase/HBaseRead.scala
@@ -1,10 +1,14 @@
 package com.tian.day04.hbase
 
-import org.apache.hadoop.hbase.{CellUtil, HBaseConfiguration}
+import java.util
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.hbase.{Cell, CellUtil, HBaseConfiguration}
 import org.apache.hadoop.hbase.client.Result
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat
 import org.apache.hadoop.hbase.util.Bytes
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -15,25 +19,24 @@ import org.apache.spark.{SparkConf, SparkContext}
 // TODO 建表验证
 object HBaseRead {
     def main(args: Array[String]): Unit = {
-        val conf = new SparkConf().setAppName("Practice").setMaster("local[2]")
-        val sc = new SparkContext(conf)
-        val hbaseConf = HBaseConfiguration.create()
+        val conf: SparkConf = new SparkConf().setAppName("Practice").setMaster("local[2]")
+        val sc: SparkContext = new SparkContext(conf)
+        val hbaseConf: Configuration = HBaseConfiguration.create()
         hbaseConf.set("hbase.zookeeper.quorum", "hadoop102,hadoop103,hadoop104")
-        hbaseConf.set(TableInputFormat.INPUT_TABLE, "student")
-        val rdd1 = sc.newAPIHadoopRDD(
+        hbaseConf.set(TableInputFormat.INPUT_TABLE, "stu")
+        val rdd1: RDD[(ImmutableBytesWritable, Result)] = sc.newAPIHadoopRDD(
             hbaseConf,
             classOf[TableInputFormat],
             classOf[ImmutableBytesWritable],
             classOf[Result]
         )
-        val rdd2 = rdd1.map {
-            case (key, result) => {
-                val cells = result.listCells()
+        val rdd2: RDD[Unit] = rdd1.map {
+            case (_, result) =>
+                val cells: util.List[Cell] = result.listCells()
                 import scala.collection.JavaConversions._
                 for (cell <- cells) {
                     println(Bytes.toString(CellUtil.cloneQualifier(cell)))
                 }
-            }
         }
         rdd2.collect.foreach(println)
         sc.stop()
--- spark/spark-core/src/main/scala/com/tian/day04/hbase/HBaseWrite.scala
+++ spark/spark-core/src/main/scala/com/tian/day04/hbase/HBaseWrite.scala
@@ -1,11 +1,13 @@
 package com.tian.day04.hbase
 
+import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.hbase.HBaseConfiguration
 import org.apache.hadoop.hbase.client.Put
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
 import org.apache.hadoop.hbase.mapreduce.TableOutputFormat
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.hadoop.mapreduce.Job
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -15,28 +17,28 @@ import org.apache.spark.{SparkConf, SparkContext}
  */
 object HBaseWrite {
     def main(args: Array[String]): Unit = {
-        val conf = new SparkConf().setAppName("Practice").setMaster("local[2]")
-        val sc = new SparkContext(conf)
-        val hbaseConf = HBaseConfiguration.create()
+        val conf: SparkConf = new SparkConf().setAppName("Practice").setMaster("local[2]")
+        val sc: SparkContext = new SparkContext(conf)
+        val hbaseConf: Configuration = HBaseConfiguration.create()
         hbaseConf.set("hbase.zookeeper.quorum", "hadoop102,hadoop103,hadoop104")
         hbaseConf.set(TableOutputFormat.OUTPUT_TABLE, "student")
         //通过job来设置输出的格式的类
-        val job = Job.getInstance(hbaseConf)
+        val job: Job = Job.getInstance(hbaseConf)
         job.setOutputFormatClass(classOf[TableOutputFormat[ImmutableBytesWritable]])
         job.setOutputKeyClass(classOf[ImmutableBytesWritable])
         job.setOutputValueClass(classOf[Put])
 
-        val initialRDD = sc.parallelize(List(("10", "apple", "11"), ("20", "banana", "21")))
-        val hbaseRDD = initialRDD.map {
-            case (row, name, weight) => {
+        val initialRDD: RDD[(String, String, String)] =
+            sc.parallelize(List(("10", "apple", "11"), ("20", "banana", "21")))
+        val hbaseRDD: RDD[(ImmutableBytesWritable, Put)] = initialRDD.map {
+            case (row, name, weight) =>
                 //封装rowKey
-                val rowKey = new ImmutableBytesWritable()
+                val rowKey: ImmutableBytesWritable = new ImmutableBytesWritable()
                 rowKey.set(Bytes.toBytes(row))
-                val put = new Put(Bytes.toBytes(row))
+                val put: Put = new Put(Bytes.toBytes(row))
                 put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("name"), Bytes.toBytes(name))
                 put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("weight"), Bytes.toBytes(weight))
                 (rowKey, put)
-            }
         }
         hbaseRDD.saveAsNewAPIHadoopDataset(job.getConfiguration)
         sc.stop()
