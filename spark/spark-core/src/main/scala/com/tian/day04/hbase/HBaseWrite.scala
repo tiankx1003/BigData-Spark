59ac1a8b8778b31a4f6440df3e372a52589acef9
Tiankx1003 <tiankx1003@gmail.com>
2019-09-18 08:43:20 +0800

    file read & write

--- spark/spark-core/src/main/scala/com/tian/day04/hbase/HBaseWrite.scala
+++ spark/spark-core/src/main/scala/com/tian/day04/hbase/HBaseWrite.scala
@@ -1,7 +1,11 @@
 package com.tian.day04.hbase
 
 import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.client.Put
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
 import org.apache.hadoop.hbase.mapreduce.TableOutputFormat
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.mapreduce.Job
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -17,6 +21,24 @@ object HBaseWrite {
         hbaseConf.set("hbase.zookeeper.quorum", "hadoop102,hadoop103,hadoop104")
         hbaseConf.set(TableOutputFormat.OUTPUT_TABLE, "student")
         //通过job来设置输出的格式的类
-        //TODO 当前进度
+        val job = Job.getInstance(hbaseConf)
+        job.setOutputFormatClass(classOf[TableOutputFormat[ImmutableBytesWritable]])
+        job.setOutputKeyClass(classOf[ImmutableBytesWritable])
+        job.setOutputValueClass(classOf[Put])
+
+        val initialRDD = sc.parallelize(List(("10", "apple", "11"), ("20", "banana", "21")))
+        val hbaseRDD = initialRDD.map {
+            case (row, name, weight) => {
+                //封装rowKey
+                val rowKey = new ImmutableBytesWritable()
+                rowKey.set(Bytes.toBytes(row))
+                val put = new Put(Bytes.toBytes(row))
+                put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("name"), Bytes.toBytes(name))
+                put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("weight"), Bytes.toBytes(weight))
+                (rowKey, put)
+            }
+        }
+        hbaseRDD.saveAsNewAPIHadoopDataset(job.getConfiguration)
+        sc.stop()
     }
 }
